6509f1653ed6798a713c1e1ccc123d679d95e25d
diamondniam <1yungdiamond@gmail.com>
2025-07-10 01:51:12 +0400

    fix

--- src/components/ui/Preloader/PreloaderContainer.tsx
+++ src/components/ui/Preloader/PreloaderContainer.tsx
@@ -1,5 +1,9 @@
 export const animatedPreloaderLineId = "animated-preloader-line";
 
-export default function PreloaderContainer({ children }) {
+export default function PreloaderContainer({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return <div className="flex items-center justify-center">{children}</div>;
 }
